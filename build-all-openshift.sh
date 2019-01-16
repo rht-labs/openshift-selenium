97ef0dbdbb658fe8a844b4246c044077959eefb4
Deven Phillips <deven.phillips@gmail.com>
2019-01-16 11:18:46 -0500

    Fix syntax error in Dockerfiles

--- build-all-openshift.sh
+++ build-all-openshift.sh
@@ -6,5 +6,5 @@ oc status
 
 for I in selenium-base selenium-hub selenium-node-base selenium-node-chrome selenium-node-chrome-debug selenium-node-firefox selenium-node-firefox-debug
 do
-  oc process --local -f openshift-templates/selenium-builds.yaml --param-file=params/${I} | oc apply -f -
+  oc process --local -f openshift-templates/selenium-builds.yaml --param-file params/${I} | oc apply -f -
 done

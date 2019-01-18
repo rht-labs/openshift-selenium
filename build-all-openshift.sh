94c7e2c33f5db211e39ab45cbddf4b8c621d9f9e
Deven Phillips <deven.phillips@gmail.com>
2019-01-18 06:20:51 -0500

    Re-work templates and params using ImageStreamTag

--- build-all-openshift.sh
+++ build-all-openshift.sh
@@ -6,5 +6,9 @@ oc status
 
 for I in selenium-base selenium-hub selenium-node-base selenium-node-chrome selenium-node-chrome-debug selenium-node-firefox selenium-node-firefox-debug
 do
-  oc process --local -f openshift-templates/selenium-builds.yaml --param-file params/${I} | oc apply -f -
+  if [[ "${I}" == "selenium-base" ]]; then
+    oc process -f openshift-templates/docker-build-with-namespace.yaml --param-file params/${I} | oc apply -f -
+  else
+    oc process -f openshift-templates/docker-build-without-namespace.yaml --param-file params/${I} | oc apply -f -
+  fi
 done

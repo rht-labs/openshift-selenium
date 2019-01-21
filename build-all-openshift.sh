797af84e0253fbd6ee0c210c2e0dfd6c78d3f015
Deven Phillips <deven.phillips@gmail.com>
2019-01-21 09:19:43 -0500

    Removed namespace param to default to current namespace

--- build-all-openshift.sh
+++ build-all-openshift.sh
@@ -2,13 +2,23 @@
 
 set -e
 
-oc status
+NAMESPACE=$(oc project | awk -F'"' '{print $2}')
 
-for I in selenium-base selenium-hub selenium-node-base selenium-node-chrome selenium-node-chrome-debug selenium-node-firefox selenium-node-firefox-debug
-do
-  if [[ "${I}" == "selenium-base" ]]; then
-    oc process -f openshift-templates/docker-build-with-namespace.yaml --param-file params/${I} | oc apply -f -
-  else
-    oc process -f openshift-templates/docker-build-without-namespace.yaml --param-file params/${I} | oc apply -f -
-  fi
-done
+read -p "Preparing to deploy Selenium Grid to namespace '${NAMESPACE}'. Is that OK (y/N)? " confirmation
+IS_CONFIRMED=$( echo ${confirmation:-n} | tr A-Z a-z )
+
+if [[ "${IS_CONFIRMED}X" == "yX" ]]; then
+
+  for I in selenium-base selenium-hub selenium-node-base selenium-node-chrome selenium-node-chrome-debug selenium-node-firefox selenium-node-firefox-debug
+  do
+    if [[ "${I}" == "selenium-base" ]]; then
+      oc process -f openshift-templates/docker-build-with-namespace.yaml --param-file params/${I} | oc apply -f -
+    else
+      oc process -f openshift-templates/docker-build-without-namespace.yaml --param-file params/${I} | oc apply -f -
+    fi
+  done
+
+  oc process -f openshift-templates/selenium-deployment.yaml --param="SELENIUM_NAMESPACE=test-deven-selenium" -o yaml | oc apply -f -
+else
+  echo "Aborting"
+fi
